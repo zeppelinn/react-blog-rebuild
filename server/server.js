e9e9d41ed6feba02355b8354d93ae578cb2fe026
ljn <467337035@qq.com>
2018-11-15 11:19:43 +0800

    完成生产环境下服务端渲染的配置

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const favicon = require('serve-favicon')
-const ReactSSR = require('react-dom/server')
+const serverRender = require('./util/server-render')
 const fs = require('fs')
 const path = require('path')
 
@@ -11,18 +11,23 @@ const app = express()
 app.use(favicon(path.join(__dirname, "../favicon.ico")))
 
 if(!isDev){
-    const serverEntry = require('../dist/server-entry').default
-    const template = fs.readFileSync(path.join(__dirname, "../dist/index.html"), 'utf8')
+    const serverEntry = require('../dist/server-entry')
+    const template = fs.readFileSync(path.join(__dirname, "../dist/server.ejs"), 'utf8')
     app.use('/public', express.static(path.join(__dirname, "../dist")))
-    app.get("*", function(req, res){
-        const appString = ReactSSR.renderToString(serverEntry)
-        const replacement = template.replace('<!-- app -->', appString)
-        res.send(replacement)
+    app.get("*", function(req, res, next){
+      serverRender(serverEntry, template, req, res)
+        .catch(next)
     })
 }else{
     const devStatic = require('./util/dev-static')
     devStatic(app)
 }
+
+// 处理报错
+app.use(function(err, req, res, next){
+  console.log(err)
+  res.status(500).send(err)
+})
 
 app.listen(3001, () => {
     console.log('sample SSR start on port 3001')
